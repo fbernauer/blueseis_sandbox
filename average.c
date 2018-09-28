8e41347e001620ab3e06f707f803cd7cf6afe95a
fbernauer <32566875+fbernauer@users.noreply.github.com>
2018-09-28 10:31:12 +0200

    Update average.c

--- average.c
+++ average.c
@@ -19,12 +19,12 @@ int nx2;
   {   
        sum1=0.0;
        sum2=0.0;
-       for(bk=0;bk<=nx2;bk++)
+       for(bk=1;bk<=nx2;bk++)
        {
             sum1=sum1+(*(tr + uk-bk));
             sum2=sum2+(*(tr + uk+bk));
         }
-        *(buffer+uk) = (sum1+sum2+(*(tr+uk)))/(float)(2*nx2+2);
+        *(buffer+uk) = (sum1+sum2+(*(tr+uk)))/(float)(2*nx2+1);
   }
  
   for(uk=1;uk<=nx2-1;uk++)
@@ -37,7 +37,7 @@ int nx2;
            sum2=sum2+(*(tr + uk+bk));
            ck=bk;
         }
-        *(buffer+uk) = (sum1+sum2+(*(tr+uk)))/(float)(2*ck+2);
+        *(buffer+uk) = (sum1+sum2+(*(tr+uk)))/(float)(2*ck+1);
   }
  
   for(uk=1;uk<=nx2-1;uk++)
@@ -51,7 +51,7 @@ int nx2;
             ck=bk;
        }
        *(buffer+windowlen-1-uk) =
-            (sum1+sum2+(*(tr+windowlen-1-uk)))/(float)(2*ck+2);
+            (sum1+sum2+(*(tr+windowlen-1-uk)))/(float)(2*ck+1);
   }
  
   *(buffer+windowlen-1) = *(tr+windowlen-1);
